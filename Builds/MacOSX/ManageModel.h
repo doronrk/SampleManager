9e4e5c4edd8f30a900271a1a7e67143943155166
doronrk <doronrk@gmail.com>
2016-05-02 12:00:26 -0700

    added model files

---
+++ Builds/MacOSX/ManageModel.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    ManageModel.h
+    Created: 2 May 2016 11:58:45am
+    Author:  Doron Roberts-Kedes
+
+  ==============================================================================
+*/
+
+#ifndef MANAGEMODEL_H_INCLUDED
+#define MANAGEMODEL_H_INCLUDED
+
+
+
+
+
+#endif  // MANAGEMODEL_H_INCLUDED
